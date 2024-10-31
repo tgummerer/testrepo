4f2d93c52e100584df2a17b26b51f642a8e19faf
Thomas Gummerer <t.gummerer@gmail.com>
2024-10-31 15:26:22 +0100

    another

--- main.go
+++ main.go
@@ -9,14 +9,13 @@ func main() {
 	ticker := time.NewTicker(20 * time.Second)
 	totalDuration := 10 * time.Minute
 	start := time.Now()
-	for i := 0; ; i++ {
-		select {
-		case <-ticker.C:
-			fmt.Println("Hello, World!", time.Now())
-			if time.Since(start) > totalDuration {
-				fmt.Println("Total duration reached", i, "expected", totalDuration/20*time.Second)
-				return
-			}
+	i := 0
+	for range ticker.C {
+		i++
+		fmt.Println("Hello, World!", time.Now())
+		if time.Since(start) > totalDuration {
+			fmt.Println("Total duration reached", i, "expected", totalDuration/20*time.Second)
+			return
 		}
 	}
 }
